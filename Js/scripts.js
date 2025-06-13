6e0d360d68f3eea616e8e7f407874450159c529c
Jhonatan Moises Villca <142120469+Bronstor@users.noreply.github.com>
2025-06-13 00:50:50 -0400

    Delete Js directory

--- Js/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const botones = document.querySelectorAll('.btn');
-  botones.forEach(btn => {
-    btn.addEventListener('click', () => {
-      alert('Función no disponible en versión estática.');
-    });
-  });
-});
